60bd91dd4cc6bc729300d462bd667e12d14755d4
MarvelHendry12 <marvelhendry112@gmail.com>
2025-06-01 12:53:55 +0700

    Update AppNavigator.java

--- AppNavigator.java
+++ AppNavigator.java
@@ -4,9 +4,19 @@ import javafx.stage.Stage;
 
 public class AppNavigator {
 	public static Stage mainStage;
+	
+	private static Homepage homepage;
 
     public static void setStage(Stage stage) {
         mainStage = stage;
+    }
+    
+    public static void setHomepageInstance(Homepage hp) {
+        homepage = hp;
+    }
+
+    public static Homepage getHomepageInstance() {
+        return homepage;
     }
 
     public static void goToLogin() {
@@ -17,5 +27,10 @@ public class AppNavigator {
     public static void goToHome() {
         Homepage home = new Homepage();
         home.start(mainStage);
+    }
+    
+    public static void goToChrume() {
+    	ChrumeApp chrume = new ChrumeApp();
+    	chrume.start(mainStage);
     }
 }
