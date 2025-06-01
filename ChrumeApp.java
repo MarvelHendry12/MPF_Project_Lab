878bb1dbc0854aec7909dd46c402b2a7a1abf80d
MarvelHendry12 <marvelhendry112@gmail.com>
2025-06-01 11:32:33 +0700

    Update ChrumeApp.java

--- ChrumeApp.java
+++ ChrumeApp.java
@@ -1,6 +1,7 @@
 package main;
 
 import javafx.geometry.Insets;
+
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -15,188 +16,328 @@ import javafx.stage.Stage;
 import java.io.File;
 
 public class ChrumeApp {
-    Scene scene;
-    BorderPane root;
-    TextField searchBar;
-    Button searchButton;
-    VBox contentBox;
-    MediaPlayer currentMediaPlayer;
 
-    public void start(Stage ps) {
-        root = new BorderPane();
-        root.setPadding(new Insets(20));
+	Scene sc; 
+	StackPane sp;
+	BorderPane bp;
+	VBox contentBox;
+	HBox searchBox;
+	TextField searchBar;
+	Button searchButton;
+	ScrollPane scrollPane;
+	MediaPlayer mediaPlayer;
 
-        // Load background image
-//        Image bgImg = new Image(getClass().getResourceAsStream("/assets/homepage.jpg"));
-//        ImageView ivBG = new ImageView(bgImg);
-//        ivBG.setPreserveRatio(false);
-//        ivBG.setFitWidth(1300);
-//        ivBG.setFitHeight(900);
+    public void init() {
+        sp = new StackPane();
+        bp = new BorderPane();
 
-        // Top search bar
-        HBox searchBox = new HBox(10);
         searchBar = new TextField();
-        searchButton = new Button("Search");
-        searchBox.getChildren().addAll(searchBar, searchButton);
-        searchBox.setAlignment(Pos.CENTER);
         searchBar.setPrefWidth(400);
 
-        // Content area
+        searchButton = new Button("Search");
+
+        searchBox = new HBox(searchBar, searchButton);
+        searchBox.setAlignment(Pos.CENTER);
+
         contentBox = new VBox(10);
         contentBox.setAlignment(Pos.CENTER);
-        ScrollPane scrollPane = new ScrollPane(contentBox);
+
+        scrollPane = new ScrollPane(contentBox);
         scrollPane.setFitToWidth(true);
-        scrollPane.setStyle("-fx-background-color: transparent;");
 
-        root.setTop(searchBox);
-        root.setCenter(scrollPane);
+        bp.setTop(searchBox);
+        bp.setCenter(scrollPane);
 
-        // StackPane untuk background + aplikasi
-        StackPane sp = new StackPane();
-        scene = new Scene(sp, 1000, 600);
-//        ivBG.fitWidthProperty().bind(scene.widthProperty());
-//        ivBG.fitHeightProperty().bind(scene.heightProperty());
-
-        sp.getChildren().addAll(root);
-
-        setSearchAction();
-
-        ps.setTitle("ChRUme");
-        ps.setScene(scene);
-        ps.show();
+        sp.getChildren().add(bp);
+        sc = new Scene(sp, 1000, 600);
+        sc.getStylesheets().add("style/ChrumeApp.css");
     }
 
-
-    private void setSearchAction() {
-        searchButton.setOnAction(e -> handleSearch());
-        searchBar.setOnAction(e -> handleSearch());
+    public void setStyle() {
+       scrollPane.getStyleClass().add("scroll-pane-transparent");
     }
 
-    private void handleSearch() {
+    public void setAction() {
+        searchButton.setOnAction(e -> search());
+        searchBar.setOnAction(e -> search());
+        
+    }
+
+    public void search() {
         String query = searchBar.getText().trim();
         contentBox.getChildren().clear();
-        stopCurrentMedia();
-
-        if (query.isEmpty()) {
-            showEmptyWebsite();
+        stopMedia(); 
+        
+        if(query.isEmpty()) {
+        	stopMedia();
         } else if (query.equals("RUtube.net")) {
-            showRUtube();
+            rutube();
         } else if (query.equals("RUtify.net")) {
-            showRUtify();
+            rutify();
         } else if (query.equals("stockimages.net")) {
-            showStockImages();
+            stockImage();
         } else {
-            showDomainNotFound(query);
+            isNotFound(query);
         }
     }
 
-    private void showEmptyWebsite() {
-        contentBox.getChildren().add(new Label("Empty Website"));
+    public void isNotFound(String domain) {
+    	Label errorLbl = new Label("This site can’t be reached");
+        Label errorSubLbl = new Label(domain + " does not exist, try checking your spelling");
+        
+        errorLbl.getStyleClass().add("error-title");
+        errorSubLbl.getStyleClass().add("error-subtitle");
+        
+        searchBox.setStyle("-fx-background-color: #f2f2f2;");
+        contentBox.setStyle("-fx-background-color: #f2f2f2;");
+        
+        VBox vb = new VBox(errorLbl, errorSubLbl);
+        
+        vb.setAlignment(Pos.CENTER);
+        
+        StackPane centeredPane = new StackPane(vb);
+        centeredPane.setPrefHeight(700); 
+        
+        contentBox.getChildren().add(centeredPane);
     }
+    
+    private void rutube() {
+        stopMedia(); 
 
-    private void showDomainNotFound(String domain) {
-        Label title = new Label("This site can’t be reached");
-        Label subtitle = new Label("The domain \"" + domain + "\" does not exist.");
-        VBox box = new VBox(10, title, subtitle);
-        box.setAlignment(Pos.CENTER);
-        contentBox.getChildren().add(box);
-    }
-
-    private void showRUtube() {
-        File file = new File("assets/DiamondJack.mp4");
+        File file = new File("src/assets/DiamondJack.mp4");
         Media media = new Media(file.toURI().toString());
-        currentMediaPlayer = new MediaPlayer(media);
-        MediaView mediaView = new MediaView(currentMediaPlayer);
+        mediaPlayer = new MediaPlayer(media);
+        MediaView mediaView = new MediaView(mediaPlayer);
+
+        Image imgRutube = new Image(getClass().getResourceAsStream("/assets/youtube-logo.png"));
+        ImageView ivRutube = new ImageView(imgRutube);
+        ivRutube.setFitHeight(40);
+        ivRutube.setFitWidth(60);
+
+        Label rutubeLbl = new Label("RUtube");
+        rutubeLbl.getStyleClass().add("rutube-label");
+
+        HBox rutubeHB = new HBox(5, ivRutube, rutubeLbl);
+        rutubeHB.setAlignment(Pos.TOP_LEFT);
+        rutubeHB.setPadding(new Insets(10));
 
         Button playBtn = new Button("Play");
         Button pauseBtn = new Button("Pause");
-        playBtn.setOnAction(e -> currentMediaPlayer.play());
-        pauseBtn.setOnAction(e -> currentMediaPlayer.pause());
+       
+        HBox controlsHB = new HBox(playBtn, pauseBtn);
+        controlsHB.setSpacing(0); 
+        controlsHB.setAlignment(Pos.CENTER);
+        controlsHB.getStyleClass().add("video-controls");
+       
+        mediaView.setPreserveRatio(true);
+        mediaView.setFitWidth(900);
+        mediaView.setSmooth(true);
+        
+        VBox videoCenterBox = new VBox(mediaView, controlsHB);
+        videoCenterBox.setAlignment(Pos.CENTER);
+        videoCenterBox.setSpacing(0); 
+        
+        BorderPane videoLayout = new BorderPane();
+        videoLayout.setTop(rutubeHB);
+        videoLayout.setCenter(videoCenterBox);
+        videoLayout.setPrefHeight(900);
+        
+        searchBox.setStyle("-fx-background-color: #ffffff;");
+        
+        sp.setStyle("-fx-background-color: #2b2b2b;");
 
-        VBox mediaBox = new VBox(10, new Label("RUtube.net"), mediaView, new HBox(10, playBtn, pauseBtn));
-        mediaBox.setAlignment(Pos.CENTER);
-        contentBox.getChildren().add(mediaBox);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setFitToHeight(true); 
+        
+        contentBox.setStyle("-fx-background-color: #2b2b2b;");
+        
+        contentBox.getChildren().clear();
+        
+        contentBox.getChildren().add(videoLayout);
+        
+        playBtn.setOnAction(e -> mediaPlayer.play());
+        pauseBtn.setOnAction(e -> mediaPlayer.pause());
     }
 
-    private void showRUtify() {
-        File file = new File("assets/PromQueen.mp3");
+
+    private void rutify() {
+        stopMedia(); 
+
+        File file = new File("src/assets/PromQueen.mp3");
         Media media = new Media(file.toURI().toString());
-        currentMediaPlayer = new MediaPlayer(media);
+        mediaPlayer = new MediaPlayer(media);
+
+        Image spotifyImg = new Image(getClass().getResourceAsStream("/assets/spotify-logo.png"));
+        ImageView spotifyLogo = new ImageView(spotifyImg);
+        spotifyLogo.setFitHeight(45);
+        spotifyLogo.setFitWidth(45);
+
+        Label rutifyLabel = new Label("RUtify");
+        rutifyLabel.getStyleClass().add("rutify-label");
+
+        HBox spotifyHB = new HBox(10, spotifyLogo, rutifyLabel);
+        spotifyHB.setAlignment(Pos.CENTER_LEFT);
+//        spotifyHB.setPadding(new Insets(20, 20, 20, 20));
+        spotifyHB.getStyleClass().add("rutify-bar");
+        BorderPane.setAlignment(spotifyHB, Pos.TOP_LEFT);
 
         Slider slider = new Slider();
+        slider.getStyleClass().add("custom-slider");
         slider.setDisable(true);
 
-        currentMediaPlayer.setOnReady(() -> slider.setMax(currentMediaPlayer.getMedia().getDuration().toSeconds()));
-        currentMediaPlayer.currentTimeProperty().addListener((obs, oldTime, newTime) -> {
-            if (!slider.isDisabled()) {
-                slider.setValue(newTime.toSeconds());
-            }
+        mediaPlayer.setOnReady(() -> {
+            slider.setMax(mediaPlayer.getMedia().getDuration().toSeconds());
+        });
+
+        mediaPlayer.currentTimeProperty().addListener((obs, oldTime, newTime) -> {
+            slider.setValue(newTime.toSeconds());
         });
 
         Button playBtn = new Button("Play");
         Button pauseBtn = new Button("Pause");
-        playBtn.setOnAction(e -> currentMediaPlayer.play());
-        pauseBtn.setOnAction(e -> currentMediaPlayer.pause());
 
-        VBox audioBox = new VBox(10, new Label("RUtify.net"), slider, new HBox(10, playBtn, pauseBtn));
+        playBtn.setOnAction(e -> mediaPlayer.play());
+        pauseBtn.setOnAction(e -> mediaPlayer.pause());
+
+        HBox controls = new HBox(playBtn, pauseBtn);
+        controls.setAlignment(Pos.CENTER);
+
+        VBox audioBox = new VBox(slider, controls);
         audioBox.setAlignment(Pos.CENTER);
-        contentBox.getChildren().add(audioBox);
+        audioBox.setPadding(new Insets(50));
+        
+        BorderPane musicLayout = new BorderPane();
+        musicLayout.setTop(spotifyHB);
+        musicLayout.setCenter(audioBox);
+        musicLayout.setPrefHeight(800);
+        musicLayout.getStyleClass().add("music-layout");
+        
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setFitToHeight(true); 
+
+        searchBox.setStyle("-fx-background-color: #ffffff;");
+        sp.setStyle("-fx-background-color: #2b2b2b;");
+        
+        contentBox.getStyleClass().add("rutify-background");
+       
+        contentBox.getChildren().clear();
+        contentBox.getChildren().add(musicLayout); 
     }
+    
+    private void stockImage() {
+        contentBox.getChildren().clear();
+        contentBox.setStyle("-fx-background-color: #000000;");
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
 
-    private void showStockImages() {
-        VBox imagesBox = new VBox(20);
-        imagesBox.setAlignment(Pos.CENTER);
+        String[][] imageData = {
+            {"cat-image1.jpg", "orangecat.jpg"},
+            {"cat-image2.jpg", "graycat.jpg"},
+            {"cat-image3.jpeg", "blackcat.jpg"},
+            {"cat-image4.jpeg", "graycat2.jpg"}
+        };
 
-        String[] imageFiles = {"cat-image1.jpg", "cat-image2.jpg", "cat-image3.jpeg", "cat-image4.jpeg"};
-        String[] names = {"orangecat", "graycat", "blackcat", "graycat2"};
+        for (String[] data : imageData) {
+            String assetFile = data[0];
+            String defaultName = data[1];
 
-        for (int i = 0; i < imageFiles.length; i++) {
-            String fileName = imageFiles[i];
-            String defaultName = names[i] + ".jpg";
+            // Gambar
+            Image image = new Image(getClass().getResourceAsStream("/assets/" + assetFile));
+            ImageView imageView = new ImageView(image);
+            imageView.setPreserveRatio(false);
+            imageView.setFitHeight(500);
+            imageView.setFitWidth(700);
 
-            ImageView imageView = new ImageView("file:assets/" + fileName);
-            imageView.setFitWidth(200);
-            imageView.setPreserveRatio(true);
+            // Tombol Download
+            Button downloadButton = new Button("Download");
 
-            Button downloadBtn = new Button("Download");
-            int finalI = i;
-            downloadBtn.setOnAction(e -> {
+            downloadButton.setOnAction(e -> {
                 TextInputDialog dialog = new TextInputDialog(defaultName);
-                dialog.setTitle("Download Image");
-                dialog.setHeaderText("Enter filename to save:");
+                dialog.setTitle("Save Shortcut");
+                dialog.setHeaderText("Enter a name for the shortcut");
+                dialog.setContentText("Name:");
+
                 dialog.showAndWait().ifPresent(name -> {
-                    if (!name.matches("[a-zA-Z0-9]+\\.jpg")) {
-                        showAlert("Filename must be alphanumeric and end with .jpg");
+                    String filename = name.trim();
+
+                    if (filename.isEmpty()) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR, "Filename cannot be empty.");
+                        alert.show();
                         return;
                     }
-                    File target = new File("downloads/" + name);
-                    if (target.exists()) {
-                        showAlert("Filename already exists!");
+
+                    // Tambahkan .jpg jika belum ada
+                    if (!filename.toLowerCase().endsWith(".jpg")) {
+                        filename += ".jpg";
+                    }
+
+                    // Ambil nama tanpa .jpg
+                    String nameWithoutExt = filename.substring(0, filename.length() - 4);
+
+                    // Cek alphanumeric
+                    boolean isAlphanumeric = true;
+                    for (char c : nameWithoutExt.toCharArray()) {
+                        if (!Character.isLetterOrDigit(c)) {
+                            isAlphanumeric = false;
+                            break;
+                        }
+                    }
+
+                    if (!isAlphanumeric) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("Error");
+                        alert.setHeaderText("File name is invalid");
+                        alert.setContentText("File name must be alphanumeric!");
+                        alert.show();
                         return;
                     }
-                    // Simulasi penyimpanan dan shortcut
-                    System.out.println("Downloaded as: " + name);
+
+                    if (Homepage.exists(filename)) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("Error");
+                        alert.setHeaderText("File with that name already exists!");
+                        alert.setContentText("A file with that name has already been made");
+                        alert.show();
+                        return;
+                    }
+
+                    Homepage.addShortcut(image, filename);
+
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Success");
+                    alert.setHeaderText("Success!");
+                    alert.setContentText("This file has been added to homepage.");
+                    
+                    alert.show();
                 });
             });
 
-            VBox imgBox = new VBox(10, imageView, downloadBtn);
-            imgBox.setAlignment(Pos.CENTER);
-            imagesBox.getChildren().add(imgBox);
-        }
+            HBox imageRow = new HBox(80);
+            imageRow.setAlignment(Pos.CENTER);
+            imageRow.setPadding(new Insets(10));
+            imageRow.getChildren().addAll(imageView, downloadButton);
 
-        contentBox.getChildren().add(imagesBox);
-    }
-
-    private void stopCurrentMedia() {
-        if (currentMediaPlayer != null) {
-            currentMediaPlayer.stop();
+            contentBox.getChildren().add(imageRow);
         }
     }
 
-    private void showAlert(String msg) {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Warning");
-        alert.setHeaderText(null);
-        alert.setContentText(msg);
-    }
+
+    public void stopMedia() {
+        if(mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+
+    public void start(Stage ps) {
+        init();
+        setStyle();
+        setAction();
+        ps.setScene(sc);
+        ps.setTitle("ChRUme");
+        ps.centerOnScreen();
+        ps.setOnCloseRequest(e -> stopMedia());
+        ps.show();
+    }
 }
